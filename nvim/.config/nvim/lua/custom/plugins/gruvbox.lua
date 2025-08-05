042cab755345d5a86ba88b388c548061d2d1e0da
Gonzalo Molina <gonmolina@gmail.com>
2025-08-05 17:25:17 -0300

    neovim color rose-pine

--- nvim/.config/nvim/lua/custom/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/custom/plugins/gruvbox.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000,
-    enabled = true,
-    config = function()
-      vim.o.background = 'dark'
-      vim.cmd [[colorscheme gruvbox]]
-    end,
-  },
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		enabled = false,
+		config = function()
+			vim.o.background = "dark"
+			vim.cmd([[colorscheme gruvbox]])
+		end,
+	},
 }
