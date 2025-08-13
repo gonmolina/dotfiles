ff1421feb804dc78d283c00201b5e9ebcef84cfa
Gonzalo Molina <gonmolina@gmail.com>
2025-08-13 09:12:46 -0300

    add fnm to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,6 +10,18 @@ $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
 # set default editor
 set -gx EDITOR "/usr/bin/nvim"
 
-# alises
+# alises eza (ls clone)
 alias ls='eza'
 alias l='eza -l'
+
+# aliases work
+alias cneaproj="cd $HOME/dev/CNEAProjects"
+alias gIOproj="cd $HOME/dev/CNEAProjects/globalIOProj/"
+alias conRA10plcio="ssh -Y -p 2222 root@ra10plcio.ctrad.control"
+
+#alias neovim
+alias obsi="cd /home/gonza/vaults/notes/ && nvim ."
+
+# config fast node manager fnm
+fnm completions --shell fish | source
+fnm env --use-on-cd --shell fish | source
