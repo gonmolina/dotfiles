f752f4b13fa8ea5f0b723c9d056ffa580b411e20
Gonzalo Molina <gonmolina@gmail.com>
2025-08-12 22:10:17 -0300

    add tmux config

--- nvim/.config/nvim/lua/custom/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/custom/plugins/rose-pine.lua
@@ -4,7 +4,8 @@ return {
 		name = "rose-pine",
 		enabled = true,
 		config = function()
-			vim.cmd("colorscheme rose-pine-moon")
+			-- vim.cmd("colorscheme rose-pine-moon")
+			vim.cmd("colorscheme rose-pine")
 		end,
 	},
 }
