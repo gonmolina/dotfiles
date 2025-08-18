8900b604ba6f76729fb59750e05eb7d7a629191d
Gonzalo Molina <gonmolina@gmail.com>
2025-08-18 08:12:25 -0300

    fix pallas function

--- fish/.config/fish/functions/pallas.fish
+++ fish/.config/fish/functions/pallas.fish
@@ -3,5 +3,5 @@ function pallas
     set -x PYTHONPATH "$pallas_folder/pyblock:$pallas_folder/pywd:$pallas_folder/webdiagram:$pallas_folder/pyelec:$pallas_folder/lib_pallas"
     echo "PYTHONPATH set at $PYTHONPATH"
     cd $HOME/dev/CNEAProjects/pallas/ 
-  git@github.com:gonmolina/laboratorio_pds.git  mamba shell webdiagram
+    micromamba activate webdiagram
 end
