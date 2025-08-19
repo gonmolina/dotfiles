e0cdb90e585b02262cc27b34111ddafc888007e1
Gonzalo Molina <gonmolina@gmail.com>
2025-08-19 11:17:01 -0300

    change shell for env activation

--- fish/.config/fish/functions/pallas.fish
+++ fish/.config/fish/functions/pallas.fish
@@ -2,36 +2,51 @@
 function pallas
     set -x PALLAS_FOLDER "$HOME/dev/CNEAProjects/pallas" 
     set -x PYTHONPATH "$PALLAS_FOLDER/pyblock:$PALLAS_FOLDER/pywd:$PALLAS_FOLDER/webdiagram:$PALLAS_FOLDER/pyelec:$PALLAS_FOLDER/lib_pallas"
+
+
     # echo "PYTHONPATH set at $PYTHONPATH"
     cd $HOME/dev/CNEAProjects/pallas/
 
     # Nombre de la sesión de tmux
-    set session_name "pallas"
+    set -gx session_name "pallas"
 
     # Nombre del entorno de Conda que quieres activar
-    set conda_env_name "webdiagram"
+    set -gx conda_env_name "webdiagram"
 
     # Comando para el primer programa
-    set prog1_cmd "micromamba activate $conda_env_name && $PALLAS_FOLDER/build/src/server -m mongodb://pallasUSER:peql1234@10.73.32.33/test -db pallas -h 0.0.0.0"
+    set -gx prog11_cmd "micromamba activate $conda_env_name" 
+    set -gx prog12_cmd "$PALLAS_FOLDER/build/src/server -m mongodb://pallasUSER:peql1234@10.73.32.33/test -db pallas -h 0.0.0.0"
 
     # Comando para el segundo programa
-    set prog2_cmd "micromamba activate $conda_env_name && cd webdiagram && python webDiagram.py"
+    # set prog2_cmd "mamba shell $conda_env_name && cd webdiagram && python webDiagram.py"
+    set prog2_cmd "cd webdiagram && python webDiagram.py"
 
-    set prog3_cmd "micromamba activate $conda_env_name && nvim ."
+    set prog3_cmd "nvim ."
 
     # Conectar a la sesión o crearla si no existe
-    # tmux new-session -d -s $session_name
-    # tmux send-keys -t $session_name:0 "$prog1_cmd" Enter 
-    tmux new-session -d -s $session_name -n "server" $prog1_cmd
+    tmux new-session -d -s $session_name
+    tmux send-keys -t $session_name "$prog11_cmd" Enter 
+    tmux send-keys -t $session_name "$prog12_cmd" Enter 
+
+    # tmux new-session -d -s $session_name -n "server" $prog1_cmd
+    # tmux send-keys "$prog2_cmd" Enter
 
     # Dividir la ventana horizontalmente y ejecutar el segundo programa
-    tmux split-window -t $session_name -h $prog2_cmd
-    # tmux split-window -h -t $session_name
+    # tmux split-window -t $session_name -h $prog2_cmd
+    tmux split-window -h -t $session_name
+    tmux send-keys -t $session_name "$prog11_cmd" Enter 
     tmux send-keys "$prog2_cmd" Enter
 
     tmux new-window -t $session_name -n "dev" 
+    tmux send-keys -t $session_name "$prog11_cmd" Enter 
     tmux send-keys -t $session_name "$prog3_cmd" Enter
 
     # Conectar a la sesión para ver los paneles
     tmux attach-session -t $session_name   # micromamba activate webdiagram
 end
+
+# function pallas_client 
+#     cd $PALLAS_FOLDER 
+#     wine $PALLAS_FOLDER/pallas_model_devel_station/client_server.bat
+# end
+
