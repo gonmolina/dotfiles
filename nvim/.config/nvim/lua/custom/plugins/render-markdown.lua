efce453c68bd93e99eebe012fcdbb66ce157e839
Gonzalo Molina <gonmolina@gmail.com>
2026-06-23 12:38:37 -0300

    fix: enable render-markdown and configure robust build step for markdown-preview

--- nvim/.config/nvim/lua/custom/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/custom/plugins/render-markdown.lua
@@ -7,15 +7,12 @@ return {
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
 		opts = { completions = { lsp = { enabled = true } } },
-		enabled = false,
 	},
 	-- install without yarn or npm
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+		build = "cd app && ./install.sh",
 	},
 }
