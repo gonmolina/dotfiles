82e4f2ec49a6e515b56a4b2f51aeb3b5cb93d7c6
Gonzalo Molina <gonmolina@gmail.com>
2026-07-16 12:01:18 -0300

    change leader S for spell options

--- nvim/.config/nvim/lua/custom/spell_options.lua
+++ nvim/.config/nvim/lua/custom/spell_options.lua
@@ -9,34 +9,34 @@ vim.api.nvim_create_user_command("ToggleSpell", function()
 end, {})
 
 -- Atajo de teclado para activar/desactivar (por ejemplo, <leader>s)
-vim.keymap.set("n", "<leader>s", ":ToggleSpell<CR>", { noremap = true, silent = true, desc = "Toggle spell check" })
+vim.keymap.set("n", "<leader>S", ":ToggleSpell<CR>", { noremap = true, silent = true, desc = "Toggle spell check" })
 
 -- Configurar los idiomas que quieres usar
 vim.opt.spelllang = { "es_ar", "es", "en_us" } -- Español e inglés
 
 -- Activar solo español
-vim.keymap.set("n", "<leader>sa", function()
+vim.keymap.set("n", "<leader>Sa", function()
 	vim.opt.spell = true
 	vim.opt.spelllang = { "es", "es_ar" }
 	print("Spell check spanish activated")
 end, { desc = "Spell check spanish" })
 
 -- Activar solo inglés
-vim.keymap.set("n", "<leader>se", function()
+vim.keymap.set("n", "<leader>Se", function()
 	vim.opt.spell = true
 	vim.opt.spelllang = "en_us"
 	print("Spell check english activated")
 end, { desc = "Spell check english" })
 
 -- Activar ambos idiomas
-vim.keymap.set("n", "<leader>sb", function()
+vim.keymap.set("n", "<leader>Sb", function()
 	vim.opt.spell = true
 	vim.opt.spelllang = { "es", "es_ar", "en_us" }
 	print("Spell check spanish/english activated")
 end, { desc = "Spell check sp/en" })
 
 -- Desactivar
-vim.keymap.set("n", "<leader>so", function()
+vim.keymap.set("n", "<leader>So", function()
 	vim.opt.spell = false
 	print("Corrección ortográfica desactivada")
 end, { desc = "Spell check off" })
